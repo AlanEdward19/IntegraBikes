97270872e2329b016f448bf23e32167fbda6ad2e
Gabriel Portillo dos Santos <gabriel.portillo0706@gmail.com>
2022-12-05 20:32:56 -0300

    fix: fix tela opcoes

--- src/screens/Opcoes.js
+++ src/screens/Opcoes.js
@@ -15,60 +15,59 @@ import { BotaoLargo } from "../components/BotaoLargo";
 export const Opcoes = ({navigation}) =>
 (
         <LinearGradient colors={[cores.cinza_claro, "#f3eae8"]} start={{ x: 0, y: 0 }} end={{ x: 2, y: 1 }} style={styles.container}>
-                <View style={{}}>
-
-                </View>
-
-                <View>
 
                         <View style={styles.top}>
 
-                                <BotaoLargo
-                                        style={styles.botaoOpcoes}
-                                        title="OPÇÕES"
-                                        size={100}
-                                        textColor={cores.branco}
-                                        buttonColor={cores.black}
-                                        borderColor={cores.black}
-                                        onPress={() => navigation.navigate("Tela Principal")}
-                                />
+                                <View style={styles.botoes}>
+                                        <BotaoLargo
+                                                style={styles.botaoOpcoes}
+                                                title="OPÇÕES"
+                                                size={100}
+                                                textColor={cores.branco}
+                                                buttonColor={cores.black}
+                                                borderColor={cores.black}
+                                                onPress={() => navigation.navigate("Tela Principal")}
+                                        />
+                                </View>
                                 
-                                <BotaoLargo
-                                        style={styles.botaoPerfil}
-                                        title="PERFIL"
-                                        size={90}
-                                        textColor={cores.branco}
-                                        buttonColor={cores.azul}
-                                        borderColor={cores.azul}
-                                        onPress={() => navigation.navigate("Tela Principal")}
-                                />
+                                <View style={styles.botoes}>
+                                        <BotaoLargo
+                                                style={styles.botaoPerfil}
+                                                title="PERFIL"
+                                                size={90}
+                                                textColor={cores.branco}
+                                                buttonColor={cores.azul}
+                                                borderColor={cores.azul}
+                                                onPress={() => navigation.navigate("Tela Principal")}
+                                        />
+                                </View>
 
-                                <BotaoLargo
-                                        style={styles.botaoCarteira}
-                                        title="CARTEIRA"
-                                        size={90}
-                                        textColor={cores.branco}
-                                        buttonColor={cores.azul}
-                                        borderColor={cores.azul}
-                                        onPress={() => navigation.navigate("Tela Principal")}
-                                />
+                                <View style={styles.botoes}>
+                                        <BotaoLargo
+                                                style={styles.botaoCarteira}
+                                                title="CARTEIRA"
+                                                size={90}
+                                                textColor={cores.branco}
+                                                buttonColor={cores.azul}
+                                                borderColor={cores.azul}
+                                                onPress={() => navigation.navigate("Tela Principal")}
+                                        />
+                                </View>
+
+                                <View style={styles.botoes}>
+                                        <BotaoLargo
+                                                style={styles.botaoCarteira}
+                                                title="HISTÓRICO"
+                                                size={90}
+                                                textColor={cores.branco}
+                                                buttonColor={cores.azul}
+                                                borderColor={cores.azul}
+                                                onPress={() => navigation.navigate("Tela Principal")}
+                                        />
+                                </View>
 
                         </View>
-
-                </View>
-
-                <View style={styles.botaoHistorico}>
-
-                        <BotaoLargo
-                                title="HISTÓRICO"
-                                size={90}
-                                textColor={cores.branco}
-                                buttonColor={cores.azul}
-                                borderColor={cores.azul}
-                                onPress={() => navigation.navigate("Tela Principal")}
-                        />
-
-                </View>
+       
         </LinearGradient>
 
 )
@@ -87,7 +86,10 @@ const styles = StyleSheet.create({
         },
 
         top: {
-                top: 70
+                paddingTop: 25,
+                paddingLeft: 20,
+                paddingRight: 20,
+                
         },
         
         container: {
@@ -96,28 +98,11 @@ const styles = StyleSheet.create({
                 backgroundColor: cores.preto_claro
         },
 
-        botaoOpcoes: {
-                alignSelf: "center",
-                position: "absolute",           
-        },
-
-        botaoPerfil: {
-                alignSelf: "center",
-                position: "absolute",
-                top: 140
-        },
-
-        botaoCarteira: {
-                alignSelf: "center",
-                position: "absolute",
-                top: 230
-        },
-
-        botaoHistorico: {
-                alignSelf: "center",
-                position: "absolute",
-                bottom: 360
-        },
+        botoes: {
+                paddingBottom: 25,
+                paddingLeft: 20,
+                paddingRight: 20,
+              },
 
 
 
